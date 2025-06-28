1fee62263bf5b375bdb51ca94988b20f578efdb6
amkhafagi <amrkhafagi@live.com>
2025-06-28 23:09:24 +0300

    dsscdds

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://umjqyvuchawgoeiwgiok.supabase.co';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtanF5dnVjaGF3Z29laXdnaW9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMzE5OTAsImV4cCI6MjA2NjcwNzk5MH0.pq9laV08amghdU8eDTkQzkluI7pwsQXs9Up7nEh8mdI';
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
